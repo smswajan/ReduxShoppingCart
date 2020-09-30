2173079e0a9be9865b9623ebcb46f51094610382
Shahjahan Md Swajan <swajan.mis13.du@gmail.com>
2020-09-30 23:42:02 +0600

    Cleaned all previous code and ready to go with react-redux

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
 import React from "react";
-// components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-// items
 import cartItems from "./cart-items";
-// redux stuff
-
-// store - stores data, think of state
-// reducer - function that used to update store
-// two arguments - state, action
-// action - what happend/what update
-// return updated or old state
-
 import { createStore } from "redux";
-import { DECREASE, INCREASE } from "./actions";
+import reducer from "./reducer";
+// react-redux - provider - wraps app, connect - used in components
 // initial store
 const initialStore = {
-    count: 0,
-    name: "john",
+    cart: cartItems,
+    total: 0,
+    amount: 0,
 };
-
-// reducer
-const reducer = (state, action) => {
-    console.log({ state, action });
-    if (action.type === DECREASE) {
-        // state.count = state.count - 1;
-        return { ...state, count: state.count - 1 };
-    }
-    if (action.type === INCREASE) {
-        return { ...state, count: state.count + 1 };
-    }
-    return state;
-};
+// store
 const store = createStore(reducer, initialStore);
-store.dispatch({ type: DECREASE });
-store.dispatch({ type: INCREASE });
-store.dispatch({ type: INCREASE });
-store.dispatch({ type: INCREASE });
-store.dispatch({ type: INCREASE });
-console.log(store.getState());
 function App() {
-    // cart setup
-
     return (
         <main>
-            <Navbar cart={store.getState()} />
+            <Navbar />
             <CartContainer cart={cartItems} />
         </main>
     );
---
+++ src/reducer.js
@@ -0,0 +1,7 @@
+import { DECREASE, INCREASE } from "./actions";
+
+// reducer
+const reducer = (state, action) => {
+    return state;
+};
+export default reducer;
