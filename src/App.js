4aea684830b8d2677043f1300c59c24c72c72940
Shahjahan Md Swajan <swajan.mis13.du@gmail.com>
2020-09-30 23:29:29 +0600

    Redux basic understanding

--- src/App.js
+++ src/App.js
@@ -6,15 +6,48 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
-function App() {
-  // cart setup
+// store - stores data, think of state
+// reducer - function that used to update store
+// two arguments - state, action
+// action - what happend/what update
+// return updated or old state
 
-  return (
-    <main>
-      <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
-  );
+import { createStore } from "redux";
+import { DECREASE, INCREASE } from "./actions";
+// initial store
+const initialStore = {
+    count: 0,
+    name: "john",
+};
+
+// reducer
+const reducer = (state, action) => {
+    console.log({ state, action });
+    if (action.type === DECREASE) {
+        // state.count = state.count - 1;
+        return { ...state, count: state.count - 1 };
+    }
+    if (action.type === INCREASE) {
+        return { ...state, count: state.count + 1 };
+    }
+    return state;
+};
+const store = createStore(reducer, initialStore);
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: INCREASE });
+store.dispatch({ type: INCREASE });
+store.dispatch({ type: INCREASE });
+store.dispatch({ type: INCREASE });
+console.log(store.getState());
+function App() {
+    // cart setup
+
+    return (
+        <main>
+            <Navbar cart={store.getState()} />
+            <CartContainer cart={cartItems} />
+        </main>
+    );
 }
 
 export default App;
