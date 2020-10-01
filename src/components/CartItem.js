b769a97b56477d691c5396909116d322c4a8f686
Shahjahan Md Swajan <swajan.mis13.du@gmail.com>
2020-10-01 10:17:24 +0600

    updated reducer with increase and decrease function

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,33 +1,52 @@
 import React from "react";
+import { connect } from "react-redux";
+import { DECREASE, INCREASE, REMOVE } from "../actions";
 
-const CartItem = ({ img, title, price, amount }) => {
-  return (
-    <div className="cart-item">
-      <img src={img} alt={title} />
-      <div>
-        <h4>{title}</h4>
-        <h4 className="item-price">${price}</h4>
-        {/* remove button */}
-        <button className="remove-btn">remove</button>
-      </div>
-      <div>
-        {/* increase amount */}
-        <button className="amount-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z" />
-          </svg>
-        </button>
-        {/* amount */}
-        <p className="amount">{amount}</p>
-        {/* decrease amount */}
-        <button className="amount-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-          </svg>
-        </button>
-      </div>
-    </div>
-  );
+const CartItem = ({
+    img,
+    title,
+    price,
+    amount,
+    remove,
+    increase,
+    decrease,
+}) => {
+    return (
+        <div className="cart-item">
+            <img src={img} alt={title} />
+            <div>
+                <h4>{title}</h4>
+                <h4 className="item-price">${price}</h4>
+                {/* remove button */}
+                <button onClick={() => remove()} className="remove-btn">
+                    remove
+                </button>
+            </div>
+            <div>
+                {/* increase amount */}
+                <button onClick={() => increase()} className="amount-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z" />
+                    </svg>
+                </button>
+                {/* amount */}
+                <p className="amount">{amount}</p>
+                {/* decrease amount */}
+                <button onClick={() => decrease()} className="amount-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    );
 };
-
-export default CartItem;
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const { id, amount } = ownProps;
+    return {
+        remove: () => dispatch({ type: REMOVE, payload: { id } }),
+        increase: () => dispatch({ type: INCREASE, payload: { id, amount } }),
+        decrease: () => dispatch({ type: DECREASE, payload: { id, amount } }),
+    };
+};
+export default connect(null, mapDispatchToProps)(CartItem);
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,61 @@
-import { DECREASE, INCREASE } from "./actions";
+import { CLEAR_CART, DECREASE, INCREASE, REMOVE } from "./actions";
 
 // reducer
 const reducer = (state, action) => {
+    if (action.type === CLEAR_CART) {
+        return { ...state, cart: [] };
+    }
+    if (action.type === DECREASE) {
+        let tempCart = [];
+        if (action.payload.amount === 1) {
+            tempCart = state.cart.filter(
+                (cartItem) => cartItem.id !== action.payload.id
+            );
+        } else {
+            tempCart = state.cart.map((cartItem) => {
+                if (cartItem.id === action.payload.id) {
+                    cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+                }
+                return cartItem;
+            });
+        }
+        return { ...state, cart: tempCart };
+    }
+
+    if (action.type === INCREASE) {
+        let tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload.id) {
+                cartItem = { ...cartItem, amount: cartItem.amount + 1 };
+            }
+            return cartItem;
+        });
+        return {
+            ...state,
+            cart: tempCart,
+        };
+    }
+    if (action.type === REMOVE) {
+        return {
+            ...state,
+            cart: state.cart.filter((item) => item.id !== action.payload.id),
+        };
+    }
+
     return state;
 };
 export default reducer;
+
+// if (action.type === DECREASE) {
+//     let tempCart = [];
+//     if (action.payload.amount === 1) {
+//         console.log("delete item");
+//     } else {
+//         tempCart = state.cart.map((cartItem) => {
+//             if (cartItem.id === action.payload.id) {
+//                 cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+//             }
+//             return cartItem;
+//         });
+//     }
+//     return { ...state, tempCart };
+// }
